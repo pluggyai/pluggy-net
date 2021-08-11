b303fe668258ae88ddd1ec251920635e6e372be7
Federico Miras <miras.federico@gmail.com>
2021-08-11 14:19:30 -0300

    feat: validate credentials new method

---
+++ Pluggy.SDK/Model/ValidationError.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Pluggy.SDK.Model
+{
+    public class ValidationError
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("message")]
+        public object Message { get; set; }
+
+        [JsonProperty("parameter")]
+        public object Parameter { get; set; }
+
+        public ValidationError()
+        {
+
+        }
+
+        public ValidationError(string code, string message, string parameter)
+        {
+            Code = code;
+            Message = message;
+            Parameter = parameter;
+        }
+    }
+}
---
+++ Pluggy.SDK/Model/ValidationResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Pluggy.SDK.Model
+{
+    public class ValidationResult
+    {
+        [JsonProperty("parameters")]
+        public List<ItemParameter> Parameters { get; set; }
+        
+        [JsonProperty("errors")]
+        public List<ValidationError> Errors { get; set; }
+
+        public ValidationResult()
+        {
+        }
+
+        public ValidationResult(List<ItemParameter> parameters, List<ValidationError> errors)
+        {
+            this.Parameters = parameters;
+            this.Errors = errors;
+        }
+
+        public IDictionary<string, object> ToBody()
+        {
+            return new Dictionary<string, object>()
+            {
+                { "parameters", Parameters?.ToDictionary(x => x.Name, x => x.Value) },
+                { "errors", Errors },
+            };
+        }
+    }
+}
