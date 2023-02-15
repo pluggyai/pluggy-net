569f73947271866a7f8df55845709828a6552718
NicolasMontone <montonenicolas01@gmail.com>
2023-02-15 12:05:28 -0300

    fix(ExecutionError): add missing fields in class definition.

--- Pluggy.SDK/Model/ExecutionError.cs
+++ Pluggy.SDK/Model/ExecutionError.cs
@@ -4,11 +4,26 @@ namespace Pluggy.SDK.Model
 {
     public class ExecutionError
     {
+        // The specific execution error code.
         [JsonProperty("code")]
         public string Code { get; set; }
 
+        // A human-readable, short description of the error
         [JsonProperty("message")]
         public string Message { get; set; }
+
+        // The exact error message returned by the institution, if any was provided.
+        [JsonProperty("providerMessage")]
+        public string ProviderMessage { get; set; }
+
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; set; }
+
+        // Unstructured properties that provide additional context/information of the error.
+        // Used for some specific cases only, such as Caixa PF & PJ.
+        // see https://docs.pluggy.ai/docs/errors-validations for more info.
+        [JsonProperty("attributes")]
+        public object Attributes { get; set; }
     }
     
 	public static class ExecutionErrorCode
@@ -24,5 +39,24 @@ namespace Pluggy.SDK.Model
         public const string USER_AUTHORIZATION_PENDING = "USER_AUTHORIZATION_PENDING";
         public const string USER_AUTHORIZATION_NOT_GRANTED = "USER_AUTHORIZATION_NOT_GRANTED";
         public const string USER_INPUT_TIMEOUT = "USER_INPUT_TIMEOUT";
+    }
+
+    public static class Metadata
+    {
+        // A provider id to relate the execution with an item, for example 'user_id'. useful to match webhook notifications with items.
+        [JsonProperty("providerId")]
+        public string ProviderId { get; set; }
+
+        // If the connector is MFA, this indicates if MFA credentials are required or not to continue the current execution.
+        [JsonProperty("hasMFA")]
+        public bool HasMFA { get; set; }
+
+        // Credentials to be used in future executions. May differ or expand from the current execution credentials.
+        [JsonProperty("credentials")]
+        public object Credentials { get; set; }
+
+        // Device nickname used when device authorization is pending.
+        [JsonProperty("deviceNickname")]
+        public string DeviceNickname { get; set; }
     }
 }
