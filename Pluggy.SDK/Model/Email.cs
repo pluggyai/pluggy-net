2ce0b21929435b26644a7bc54d05bf811ff3d60c
NicolasMontone <montonenicolas01@gmail.com>
2024-09-17 13:51:32 -0300

    fix(Email): make type nullable.

--- Pluggy.SDK/Model/Email.cs
+++ Pluggy.SDK/Model/Email.cs
@@ -5,7 +5,7 @@ namespace Pluggy.SDK.Model
     public class Email
     {
         [JsonProperty("type")]
-        public EmailType Type { get; set; }
+        public EmailType? Type { get; set; }
 
         [JsonProperty("value")]
         public string Value { get; set; }
